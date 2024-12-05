147c82228e28c44e0e98ffa32ba08dc74159926c
zegameiro <jose.mcgameiro@ua.pt>
2024-12-05 12:07:28 +0000

    Added a small feature

--- ATuaVoz/src/Background.jsx
+++ ATuaVoz/src/Background.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useMemo } from "react";
 import Particles from "@tsparticles/react";
 import { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 import PropTypes from "prop-types";
 import particlesConfig from "./background.config";
+import { nyanParticles } from "./background.config";
 
 export default function Background({ children }) {
   const [init, setInit] = useState(false);
+  const [nyan, setNyan] = useState(false);
+  const [clickTimes,setClickTimes] = useState(0);
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
@@ -16,16 +19,34 @@ export default function Background({ children }) {
     });
   }, []);
 
+  const options = useMemo(() => {
+    if (nyan) {
+      return nyanParticles;
+    } else {
+      return particlesConfig;
+    }
+  }, [nyan]);
+
+  const clickTimeCheck = () => {
+    if (clickTimes < 9) {
+      setClickTimes(clickTimes + 1);
+    } else if (clickTimes === 9) {
+      setNyan(true);
+      setClickTimes(11);
+    }
+  }
+
   return (
-    <div className="relative w-full">
+    <div className="relative w-full" onClick={() => clickTimeCheck()}>
       {init && (
         <Particles
           id="tsparticles"
-          options={particlesConfig}
+          options={options}
           className="absolute -z-10"
         />
       )}
-      <div className="relative z-10">{children}</div> {/* Content overlays particles */}
+      <div className="relative z-10">{children}</div>{" "}
+      {/* Content overlays particles */}
     </div>
   );
 }
