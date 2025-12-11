813b0f2ab21619525a480f14e9d099d0d1d4dd5a
João Luís <jnluis@ua.pt>
2025-12-11 10:48:19 +0000

    Updated instagram feed

--- ATuaVoz/src/data/instagramPosts.js
+++ ATuaVoz/src/data/instagramPosts.js
@@ -1,6 +1,22 @@
 import post1 from '../assets/instagram/post1.webp';
+import post2 from '../assets/instagram/post2.webp';
+import post3 from '../assets/instagram/post3.webp';
 
 const instagramPosts = [
+    {
+    id: "3",
+    image: post3,
+    url: "https://www.instagram.com/p/DSGOeC8CDwg/?img_index=1",
+    type: "image",
+    date: "2024-12-10"
+  },
+  {
+    id: "2",
+    image: post2,
+    url: "https://www.instagram.com/p/DSF5ts8CL1b/",
+    type: "image",
+    date: "2024-12-10"
+  },
   {
     id: "1",
     image: post1,
@@ -8,6 +24,7 @@ const instagramPosts = [
     type: "image",
     date: "2024-12-04"
   },
+
 ];
 
 
