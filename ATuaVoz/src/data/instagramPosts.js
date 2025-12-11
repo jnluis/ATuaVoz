5c1e7da971c92ba7cd592c9eec5262c8a0b6447d
João Luís <jnluis@ua.pt>
2025-12-11 18:26:39 +0000

    Updated instagram feed

--- ATuaVoz/src/data/instagramPosts.js
+++ ATuaVoz/src/data/instagramPosts.js
@@ -1,9 +1,33 @@
 import post1 from '../assets/instagram/post1.webp';
 import post2 from '../assets/instagram/post2.webp';
 import post3 from '../assets/instagram/post3.webp';
+import post4 from '../assets/instagram/post4.webp';
+import post5 from '../assets/instagram/post5.webp';
+import post6 from '../assets/instagram/post6.webp';
 
 const instagramPosts = [
-    {
+  {
+    id: "6",
+    image: post6,
+    url: "https://www.instagram.com/p/DSIehhWCFqA/?img_index=1",
+    type: "image",
+    date: "2024-12-11"
+  },
+  {
+    id: "5",
+    image: post5,
+    url: "https://www.instagram.com/p/DSIULfqCNTC/?img_index=1",
+    type: "image",
+    date: "2024-12-11"
+  },
+  {
+    id: "4",
+    image: post4,
+    url: "https://www.instagram.com/p/DSH8eE0CAb9/?img_index=1",
+    type: "image",
+    date: "2024-12-11"
+  },
+  {
     id: "3",
     image: post3,
     url: "https://www.instagram.com/p/DSGOeC8CDwg/?img_index=1",
